a33b2f27b38d492b062228d2a20b56d1bdf5a215
Mann brostoch <manbrostoch@gmail.com>
2024-03-18 16:06:39 +0700

    s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DocumentComponent } from './component/document/document.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'document', component: DocumentComponent },
+];
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDPW0nVwLSTx74hvMu_i_QH8xk_8rBZ2ro",
+        authDomain: "angular-v17-58ed2.firebaseapp.com",
+        projectId: "angular-v17-58ed2",
+        storageBucket: "angular-v17-58ed2.appspot.com",
+        messagingSenderId: "13840844751",
+        appId: "1:13840844751:web:42f82a25b7cdd3838bb4ba"
+    }
+};
